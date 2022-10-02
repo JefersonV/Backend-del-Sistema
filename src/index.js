82fe81653baedc6510cdda336b5977451f3d9a0c
VictorSamines <saminesvictor@gmail.com>
2022-10-02 16:49:46 -0600

    manejador de errores general

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./routes/sales.routes");
 const morgan = require("morgan");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -8,9 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(router);
+
+app.use((err, req, res, next) => {
+  return res.json({
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
