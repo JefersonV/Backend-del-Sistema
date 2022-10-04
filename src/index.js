e30b316589655f5f16ea53612a93c1c87b8231ea
Donald <thomaschavajay20@gmail.com>
2022-10-04 16:31:29 -0600

    "Login sin consultar la DB"

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const router = require("./routes/sales.routes");
 const morgan = require("morgan");
 
+//Librerias para el login del usuario
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
+
+//Middleware para login
+app.use(cors());
+
+//Routes for login
+app.use("/login", (req, res) => {
+  res.send({
+    token: "test123",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
